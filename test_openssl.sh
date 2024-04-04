f50d8e039c6f229af3d8713c4f3d757a09d740c2
Eduin Garcia Cordero <eduinlight@gmail.com>
2024-04-04 22:17:23 +0200

    Adding color for fun

--- test_openssl.sh
+++ test_openssl.sh
@@ -9,16 +9,27 @@ FILE_PATH=./file.txt
 ENCRYPTED_PATH=./file.encrypted
 UNENCRYPTED_PATH=./file.unencrypted
 
+BLUE_COLOR="\e[34m"
+RED_COLOR="\e[31m"
+GREEN_COLOR="\e[32m"
+DEFAULT_COLOR="\e[0m"
+
 mkdir -p keys
+
+# 1 => text
+# 2 => color
+function echo-color() {
+  echo -e "${2}${1}${DEFAULT_COLOR}"
+}
 
 function gen-keys() {
   echo "GENERATING PRIVATE KEY..."
   openssl genrsa -aes128 -passout pass:$PASSPHRASE -out $PRIVATE_KEY_PATH 1024
-  echo "PRIVATE KEY GENERATED => ${PRIVATE_KEY_PATH}"
+  echo "PRIVATE KEY GENERATED => $(echo-color $PRIVATE_KEY_PATH $GREEN_COLOR)"
   echo ""
   echo "GENERATING PUBLIC KEY..."
   openssl rsa -passin pass:$PASSPHRASE -in $PRIVATE_KEY_PATH -pubout > $PUBLIC_KEY_PATH
-  echo "PUBLIC KEY GENERATED => ${PUBLIC_KEY_PATH}"
+  echo "PUBLIC KEY GENERATED => $(echo-color $PUBLIC_KEY_PATH $GREEN_COLOR)"
   echo ""
 }
 
@@ -26,9 +37,9 @@ function gen-keys() {
 # 2 => output file
 # 3 => public key
 function encrypt() {
-  echo "ENCRYPTING ${1} with ${3}"
+  echo "ENCRYPTING $(echo-color $1 $BLUE_COLOR) with $(echo-color $3 $BLUE_COLOR)"
   openssl pkeyutl -encrypt -passin pass:$PASSPHRASE -inkey $3 -pubin -in $1 -out $2
-  echo "FILE ENCRYPTED => ${2}"
+  echo "FILE ENCRYPTED => $(echo-color $2 $GREEN_COLOR)"
   echo ""
 }
 
@@ -36,20 +47,20 @@ function encrypt() {
 # 2 => output file
 # 3 => private key
 function decrypt() {
-  echo "UNENCRYPTING ${1} with ${3}"
+  echo "UNENCRYPTING $(echo-color $1 $BLUE_COLOR) with $(echo-color $3 $BLUE_COLOR)"
   openssl pkeyutl -decrypt -passin pass:$PASSPHRASE -inkey $3 -in $1 > $2
-  echo "FILE UNENCRYPTED => ${2}"
+  echo "FILE UNENCRYPTED => $(echo-color $2 $GREEN_COLOR)"
   echo ""
 }
 
 # 1 => file a
 # 2 => file b
 function compare-files() {
-  echo "COMPARING ${1} with ${2}"
+  echo "COMPARING $(echo-color $1 $BLUE_COLOR) with $(echo-color $2 $BLUE_COLOR)"
   if (diff $1 $2 -s > /dev/null); then
-    echo "✅ File ${1} and file ${2} are EQUALS"
+    echo-color "✅ File ${1} and file ${2} are EQUALS" $GREEN_COLOR
   else
-    echo "❌ File ${1} and file ${2} are DIFFERENT"
+    echo-color "❌ File ${1} and file ${2} are DIFFERENT" $RED_COLOR
   fi
 }
 
